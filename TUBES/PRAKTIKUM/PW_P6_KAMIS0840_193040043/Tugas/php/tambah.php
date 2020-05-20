a63024e214ed0cf154d34843431193d1e3966127
Herlan Nurachman <58686408+Herlan21@users.noreply.github.com>
2020-05-20 20:05:03 +0700

    Add files via upload

---
+++ TUBES/PRAKTIKUM/PW_P6_KAMIS0840_193040043/Tugas/php/tambah.php
@@ -0,0 +1,80 @@
+<?php
+
+require "function.php";
+
+if (isset($_POST['tambah'])) {
+
+  if (tambah($_POST) > 0) {
+    echo "<script>
+        alert('Data berhasil ditambah');
+        document.location.href = 'admin.php';
+    </script>";
+  } else {
+    echo "<script>
+        alert('Data gagal ditambah');
+        document.location.href = 'admin.php';
+    </script>";
+  }
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <title>Request Buku</title>
+</head>
+
+<body>
+
+  <form action="" method="POST">
+
+    <div class="form-group">
+      <label for="cover">Cover</label>
+      <input name="cover" type="text" class="form-control" aria-describedby="emailHelp" placeholder="Masukkan Nama Cover Buku">
+      <small id="emailHelp" class="form-text text-muted">Silahkan Masukkan Judul Buku Yang Akan Anda masukan</small>
+    </div>
+
+    <div class="form-group">
+      <label for="judul_buku">Judul Buku</label>
+      <input name="judul_buku" type="text" class="form-control" placeholder="Masukkan Judul Buku">
+    </div>
+
+
+    <div class="form-group">
+      <label for="pengarang">Pengarang</label>
+      <input name="pengarang" type="text" class="form-control" placeholder="Masukkan Nama Pengarang">
+    </div>
+
+
+    <div class="form-group">
+      <label for="tahun_terbit">Tahun Terbit</label>
+      <input name="tahun_terbit" type="text" class="form-control" placeholder="Masukkan Tahun Terbit Buku">
+    </div>
+
+
+    <div class="form-group">
+      <label for="harga">Harga</label>
+      <input name="harga" type="text" class="form-control" placeholder="Masukkan Harga Buku">
+    </div>
+
+    <button type="submit" class="btn btn-primary" name="tambah">Tambah Data</button>
+
+  </form>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ TUBES/PRAKTIKUM/PW_P6_KAMIS0840_193040043/latihan 6d/php/function.php
@@ -0,0 +1,74 @@
+<?php
+
+function koneksi()
+{
+
+    $conn = mysqli_connect("localhost", "root", "");
+    mysqli_select_db($conn, "tubes_193040043");
+    return $conn;
+}
+
+function query($sql)
+{
+    $conn = koneksi();
+    $result = mysqli_query($conn, "$sql");
+    $buku = [];
+    while ($buku = mysqli_fetch_assoc($result)) {
+        $bk[] = $buku;
+    }
+    return $bk;
+}
+
+function tambah($data)
+{
+    $conn = koneksi();
+    $cover = htmlspecialchars($data['cover']);
+    $judul_buku = htmlspecialchars($data['judul_buku']);
+    $pengarang = htmlspecialchars($data['pengarang']);
+    $tahun_terbit = htmlspecialchars($data['tahun_terbit']);
+    $harga = htmlspecialchars($data['harga']);
+
+    $query = "INSERT INTO buku VALUES 
+            ('','$cover' , '$judul_buku' , '$pengarang' , '$tahun_terbit' , '$harga')
+            
+    ";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+
+function hapus($id)
+{
+    $conn = koneksi();
+    mysqli_query($conn, "DELETE FROM buku WHERE id_buku = $id");
+
+    return mysqli_affected_rows($conn);
+}
+
+
+function ubah($data)
+{
+    $conn = koneksi();
+    $id_buku = htmlspecialchars($data['id_buku']);
+    $cover = htmlspecialchars($data['cover']);
+    $judul_buku = htmlspecialchars($data['judul_buku']);
+    $pengarang = htmlspecialchars($data['pengarang']);
+    $tahun_terbit = htmlspecialchars($data['tahun_terbit']);
+    $harga = htmlspecialchars($data['harga']);
+
+    $queryubah = "UPDATE buku SET 
+                cover = '$cover',
+                judul_buku = '$judul_buku',
+                pengarang = '$pengarang',
+                tahun_terbit = '$tahun_terbit',
+                harga = '$harga'   
+    WHERE id_buku = '$id_buku' 
+
+           ";
+
+    mysqli_query($conn, $queryubah);
+
+    return mysqli_affected_rows($conn);
+}
